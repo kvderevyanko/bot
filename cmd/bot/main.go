f316f1729a4b162eaa8506696db9f69d52ca6108
Kirill <kvderevyanko@gmail.com>
2026-01-11 04:13:42 +0700

    Add products

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/joho/godotenv"
+	"github.com/kvderevyanko/bot/internal/service/product"
 	"log"
 	"os"
 
@@ -30,12 +31,15 @@ func main() {
 
 	updates := bot.GetUpdatesChan(u)
 
-	for update := range updates {
-		if update.Message != nil { // If we got a message
+	productService := product.NewService()
 
+	for update := range updates {
+		if update.Message != nil {
 			switch update.Message.Command() {
 			case "help":
 				helpCommand(bot, update.Message)
+			case "list":
+				listCommand(bot, update.Message, productService)
 			default:
 				defaultBehavior(bot, update.Message)
 			}
@@ -44,13 +48,27 @@ func main() {
 }
 
 func helpCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "Что ты хочешь?")
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID,
+		"/help - help \n"+
+			"/list - list products")
+	bot.Send(msg)
+}
+
+func listCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message, productService *product.Service) {
+	outputMessageText := "Here all the products are:\n\n"
+	products := productService.List()
+	for _, p := range products {
+		outputMessageText += p.Title
+		outputMessageText += "\n"
+	}
+
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, outputMessageText)
 	bot.Send(msg)
 }
 
 func defaultBehavior(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
 	log.Printf("[%s] %s", inputMessage.From.UserName, inputMessage.Text)
 	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "Ты написал "+inputMessage.Text)
-	//msg.ReplyToMessageID = update.Message.MessageID
+	msg.ReplyToMessageID = inputMessage.MessageID //ответ на сообщение
 	bot.Send(msg)
 }

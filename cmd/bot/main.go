d2e2f5fe0aac5f5150b210159f91a743f7776b0c
Kirill <kvderevyanko@gmail.com>
2026-01-22 00:45:12 +0700

    REefactoring commands

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
+	"github.com/kvderevyanko/bot/internal/app/commands"
 	"github.com/kvderevyanko/bot/internal/service/product"
 	"log"
 	"os"
-
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func main() {
@@ -30,45 +30,18 @@ func main() {
 	}
 
 	updates := bot.GetUpdatesChan(u)
-
 	productService := product.NewService()
-
+	commander := commands.NewCommander(bot, productService)
 	for update := range updates {
 		if update.Message != nil {
 			switch update.Message.Command() {
 			case "help":
-				helpCommand(bot, update.Message)
+				commander.Help(update.Message)
 			case "list":
-				listCommand(bot, update.Message, productService)
+				commander.List(update.Message)
 			default:
-				defaultBehavior(bot, update.Message)
+				commander.Default(update.Message)
 			}
 		}
 	}
-}
-
-func helpCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID,
-		"/help - help \n"+
-			"/list - list products")
-	bot.Send(msg)
-}
-
-func listCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message, productService *product.Service) {
-	outputMessageText := "Here all the products are:\n\n"
-	products := productService.List()
-	for _, p := range products {
-		outputMessageText += p.Title
-		outputMessageText += "\n"
-	}
-
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, outputMessageText)
-	bot.Send(msg)
-}
-
-func defaultBehavior(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
-	log.Printf("[%s] %s", inputMessage.From.UserName, inputMessage.Text)
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "Ты написал "+inputMessage.Text)
-	msg.ReplyToMessageID = inputMessage.MessageID //ответ на сообщение
-	bot.Send(msg)
 }
